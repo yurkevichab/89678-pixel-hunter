6237577ddf3d7fae019cfc416e74273dce50c93a
WILDBERRIES\Yurkevich.A <yurkevich.a@wildberries.ru>
2017-05-24 14:39:11 +0300

    поменял " на '

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-﻿const mainBlock = document.querySelector("main.central");
+﻿const mainBlock = document.querySelector('main.central');
 let index = 0;
 const displays = [
-  document.getElementById("greeting"),
-  document.getElementById("rules"),
-  document.getElementById("game-1"),
-  document.getElementById("game-2"),
-  document.getElementById("game-3"),
-  document.getElementById("stats")
+  document.getElementById('greeting'),
+  document.getElementById('rules'),
+  document.getElementById('game-1'),
+  document.getElementById('game-2'),
+  document.getElementById('game-3'),
+  document.getElementById('stats')
 ];
 
 const switchDisplay = (number) => {
-  mainBlock.innerHTML = "";
+  mainBlock.innerHTML = '';
   const display = displays[number].innerHTML;
   mainBlock.innerHTML = display;
 };

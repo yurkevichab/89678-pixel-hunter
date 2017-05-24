65f70c8669a187625df1aef3da5d04c3de1c8bd2
WILDBERRIES\Yurkevich.A <yurkevich.a@wildberries.ru>
2017-05-24 14:08:51 +0300

    Добавил main.js. Добавил переключение экранов по нажатии на ALT влево, ALT вправо

---
+++ js/main.js
@@ -0,0 +1,35 @@
+﻿const mainBlock = document.querySelector("main.central");
+let index = 0;
+const displays = [
+    document.getElementById("greeting"),
+    document.getElementById("rules"),
+    document.getElementById("game-1"),
+    document.getElementById("game-2"),
+    document.getElementById("game-3"),
+    document.getElementById("stats")
+];
+
+const switchDisplay = (number) => {
+    mainBlock.innerHTML = '';
+    const display = displays[number].innerHTML;
+    mainBlock.innerHTML = display;
+};
+
+document.onkeydown = (e) => {
+    if (e.altKey && e.keyCode == 37) {
+        e.preventDefault();
+        if (index > 0) {
+            --index;
+            switchDisplay(index);
+
+        }
+    }
+    if (e.altKey && e.keyCode == 39) {
+        e.preventDefault();
+        if (index < displays.length - 1) {
+            ++index;
+            switchDisplay(index);
+
+        }
+    }
+};

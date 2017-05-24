6396a7f995cff7ad696a77eca71ee9801638f252
WILDBERRIES\Yurkevich.A <yurkevich.a@wildberries.ru>
2017-05-24 14:34:07 +0300

    поправил пробелы в файле

--- js/main.js
+++ js/main.js
@@ -1,33 +1,33 @@
 ﻿const mainBlock = document.querySelector("main.central");
 let index = 0;
 const displays = [
-    document.getElementById("greeting"),
-    document.getElementById("rules"),
-    document.getElementById("game-1"),
-    document.getElementById("game-2"),
-    document.getElementById("game-3"),
-    document.getElementById("stats")
+  document.getElementById("greeting"),
+  document.getElementById("rules"),
+  document.getElementById("game-1"),
+  document.getElementById("game-2"),
+  document.getElementById("game-3"),
+  document.getElementById("stats")
 ];
 
 const switchDisplay = (number) => {
-    mainBlock.innerHTML = '';
-    const display = displays[number].innerHTML;
-    mainBlock.innerHTML = display;
+  mainBlock.innerHTML = "";
+  const display = displays[number].innerHTML;
+  mainBlock.innerHTML = display;
 };
 
 document.onkeydown = (e) => {
-    let key = e.keyCode;
-    if (e.altKey && (key === 37 || key === 39)) {
-        e.preventDefault();
-        if (key === 37) {
-            if (index > 0) {
-                --index;
-            }
-        } else {
-            if (index < displays.length - 1) {
-                ++index;
-            }
-        }
-        switchDisplay(index);
+  const key = e.keyCode;
+  if (e.altKey && (key === 37 || key === 39)) {
+    e.preventDefault();
+    if (key === 37) {
+      if (index > 0) {
+        --index;
+      }
+    } else {
+      if (index < displays.length - 1) {
+        ++index;
+      }
     }
+    switchDisplay(index);
+  }
 };

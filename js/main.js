845c147835e07be0aac1cce7c698cc44bdcf3527
Yurkevich Alexey <yurkevichab@gmail.com>
2017-05-25 23:40:56 +0300

    поправил получение приветственного экрана. Отвельный метод для события

--- js/main.js
+++ js/main.js
@@ -2,8 +2,16 @@ const mainBlock = document.querySelector(`main.central`);
 const LEFT_BTN_KEY = 37;
 const RIGHT_BTN_KEY = 39;
 const MIN_DISPLAY_INDEX = 0;
+
 const displays = [
-  mainBlock.cloneNode(true),
+  ...[mainBlock].map((node) => {
+    const fragment = document.createDocumentFragment();
+    const main = node.querySelector(`.central__content`).cloneNode(true);
+    const footer = node.querySelector(`.footer`).cloneNode(true);
+    fragment.appendChild(main);
+    fragment.appendChild(footer);
+    return fragment;
+  }),
   ...[
     document.getElementById(`greeting`),
     document.getElementById(`rules`),
@@ -29,22 +37,20 @@ const increaseIndex = (displayIndex, maxIndex) => {
   return Math.min(++displayIndex, maxIndex);
 };
 
-document.addEventListener(`keydown`,
-    (e) => {
-      if (!e.altKey) {
-        return;
-      }
-      const currentkey = e.keyCode;
-      if (currentkey === LEFT_BTN_KEY || currentkey === RIGHT_BTN_KEY) {
-        e.preventDefault();
-        if (currentkey === LEFT_BTN_KEY) {
-          activeDisplayIndex = decreaseIndex(activeDisplayIndex, MIN_DISPLAY_INDEX);
-          switchDisplay(activeDisplayIndex);
-        }
-        if (currentkey === RIGHT_BTN_KEY) {
-          const maxDisplaysIndex = Math.max(displays.length - 1, MIN_DISPLAY_INDEX);
-          activeDisplayIndex = increaseIndex(activeDisplayIndex, maxDisplaysIndex);
-          switchDisplay(activeDisplayIndex);
-        }
-      }
-    });
+const onDocumentKeyDown = (e) => {
+  if (!e.altKey) {
+    return;
+  }
+  const currentKey = e.keyCode;
+  if (currentKey === LEFT_BTN_KEY) {
+    activeDisplayIndex = decreaseIndex(activeDisplayIndex, MIN_DISPLAY_INDEX);
+    switchDisplay(activeDisplayIndex);
+  }
+  if (currentKey === RIGHT_BTN_KEY) {
+    const maxDisplaysIndex = displays.length - 1;
+    activeDisplayIndex = increaseIndex(activeDisplayIndex, maxDisplaysIndex);
+    switchDisplay(activeDisplayIndex);
+  }
+};
+
+document.addEventListener(`keydown`, onDocumentKeyDown);

7131571e8deaca9a0a9e721f50f0b0711ab0f4ff
Yurkevich Alexey <yurkevichab@gmail.com>
2017-06-13 21:39:20 +0300

    тесты кол-ва вопросов, получение след.вопроса

--- js/data/game.test.js
+++ js/data/game.test.js
@@ -1,9 +1,10 @@
 import assert from 'assert';
 import {checkAnswer, addPoint} from './answer';
 import {setNewLives} from './lives';
+import {getNextQuestion} from './question';
 
 describe(`Game`, () => {
-  describe(`answers`, () => {
+  describe(`Answers`, () => {
     it(`should check right answer`, () => {
       const answers = [
         {
@@ -29,11 +30,12 @@ describe(`Game`, () => {
     });
   });
 
-  describe(`timer`, () => {
+  describe(`Timer`, () => {
     it(`should check fast answer`, () => {
       const state = {'timer': 25, 'stats': []};
       assert.equal(addPoint(state).stats.pop(), `fast`);
     });
+
     it(`should check slow answer`, () => {
       const state = {'timer': 8, 'stats': []};
       assert.equal(addPoint(state).stats.pop(), `slow`);
@@ -45,6 +47,7 @@ describe(`Game`, () => {
       const state = {'lives': 3};
       assert.equal(2, setNewLives(state, 2).lives);
     });
+
     it(`should throw exception max lives`, () => {
       const state = {'lives': 3};
       const maxRangeError = () => {
@@ -52,12 +55,25 @@ describe(`Game`, () => {
       };
       assert.throws(maxRangeError, Error);
     });
+
     it(`should throw exception min lives`, () => {
       const state = {'lives': 3};
       const minRangeError = () => {
         setNewLives(state, -1);
       };
       assert.throws(minRangeError, Error);
+    });
+  });
+  describe(`Questions`, () => {
+    it(`should get next question`, () => {
+      const state = {'questionNumber': 2, 'game': 0};
+      assert.notEqual(-1, getNextQuestion(state).game);
+      assert.equal(state.questionNumber + 1, getNextQuestion(state).questionNumber);
+    });
+    it(`should check is last question`, () => {
+      const state = {'questionNumber': 10, 'game': 0, 'isLastQuestion': false};
+      assert.equal(true, getNextQuestion(state).isLastQuestion);
+      assert.equal(10, getNextQuestion(state).questionNumber);
     });
   });
 });
---
+++ js/data/question.js
@@ -0,0 +1,20 @@
+import {games} from '../data';
+
+export const getNextQuestion = (state) => {
+  let gameNumber = state.game;
+  let isLastQuestion = false;
+  let questionNumber = state.questionNumber;
+  if (questionNumber === 10) {
+    isLastQuestion = true;
+  } else {
+    questionNumber += 1;
+    gameNumber = Math.floor(Math.random() * games.length);
+  }
+  const newState = Object.assign({}, state,
+      {
+        'game': gameNumber,
+        'questionNumber': questionNumber,
+        'isLastQuestion': isLastQuestion
+      });
+  return newState;
+};

564ced4d6940308c955e1b41e4e5eba15b4add3f
Yurkevich Alexey <yurkevichab@gmail.com>
2017-06-14 22:49:40 +0300

    Поправил замечания, добавил тесты

--- js/data/answer.js
+++ js/data/answer.js
@@ -1,18 +1,28 @@
 import {initialState} from '../data';
 const FAST_ANSWER = 10;
 const SLOW_ANSWER = 20;
+
 export const checkAnswer = (answer, rightAnswer) => {
   return answer.type === rightAnswer.type;
 };
 
-export const getAnswerType = (state) => {
-  const differenceTime = initialState.timer - state.timer;
-  const userStats = state.stats;
+
+export const getAnswerType = (isCorrect, {timer}) => {
+  const differenceTime = initialState.timer - timer;
+  if (!isCorrect) {
+    return `wrong`;
+  }
   if (differenceTime < FAST_ANSWER) {
-    userStats.push(`fast`);
+    return `fast`;
   }
   if (differenceTime > SLOW_ANSWER) {
-    userStats.push(`slow`);
+    return `slow`;
   }
-  return Object.assign({}, state, {stats: userStats});
+  return `correct`;
+};
+
+export const setState = (state, point) => {
+  const newState = Object.assign({}, state);
+  newState.stats.push(point);
+  return newState;
 };
--- js/data/game.test.js
+++ js/data/game.test.js
@@ -1,5 +1,5 @@
 import assert from 'assert';
-import {checkAnswer, getAnswerType} from './answer';
+import {checkAnswer, getAnswerType, setState} from './answer';
 import {setLives} from './lives';
 import {getNextQuestion} from './question';
 
@@ -30,25 +30,48 @@ describe(`Game`, () => {
     });
   });
 
-  describe(`Timer`, () => {
-    it(`should check fast answer`, () => {
-      const state = {'timer': 25, 'stats': []};
-      assert.equal(getAnswerType(state).stats.pop(), `fast`);
-    });
-
-    it(`should check slow answer`, () => {
-      const state = {'timer': 8, 'stats': []};
-      assert.equal(getAnswerType(state).stats.pop(), `slow`);
+  describe(`Add point in state`, () => {
+    it(`check add correct point to stats`, () => {
+      const state = {stats: []};
+      const lastAddPoint = setState(state, `correct`).stats.pop();
+      assert.equal(lastAddPoint, `correct`);
     });
   });
 
-  describe(`Lives`, () => {
-    it(`should update lives`, () => {
-      const state = {'lives': 3};
-      assert.equal(2, setLives(state, 2).lives);
+  describe(`Changing the timer`, () => {
+    it(`should return fast answer point`, () => {
+      const state = {'timer': 25};
+      const verifiedPoint = getAnswerType(true, state);
+      assert.equal(verifiedPoint, `fast`);
     });
 
-    it(`should throw exception max lives`, () => {
+    it(`should return slow answer type`, () => {
+      const state = {'timer': 8};
+      const verifiedPoint = getAnswerType(true, state);
+      assert.equal(verifiedPoint, `slow`);
+    });
+
+    it(`should return wrong answer type`, () => {
+      const state = {'timer': 8};
+      const verifiedPoint = getAnswerType(false, state);
+      assert.equal(verifiedPoint, `wrong`);
+    });
+
+    it(`should return correct answer type`, () => {
+      const state = {'timer': 15};
+      const verifiedPoint = getAnswerType(true, state);
+      assert.equal(verifiedPoint, `correct`);
+    });
+  });
+
+  describe(`Changing the lives`, () => {
+    it(`should check changes lives`, () => {
+      const state = {'lives': 3};
+      const verifiedLives = setLives(state, 2).lives;
+      assert.equal(2, verifiedLives);
+    });
+
+    it(`should throw exception if lives more than max possible`, () => {
       const state = {'lives': 3};
       const maxRangeError = () => {
         setLives(state, 4);
@@ -56,7 +79,7 @@ describe(`Game`, () => {
       assert.throws(maxRangeError, Error);
     });
 
-    it(`should throw exception min lives`, () => {
+    it(`should throw exception if lives less than zero`, () => {
       const state = {'lives': 3};
       const minRangeError = () => {
         setLives(state, -1);
@@ -64,17 +87,20 @@ describe(`Game`, () => {
       assert.throws(minRangeError, Error);
     });
   });
-  describe(`Questions`, () => {
+
+  describe(`Changing the question number`, () => {
     it(`should get next question`, () => {
       const state = {'questionNumber': 2, 'game': 0};
-      assert.notEqual(-1, getNextQuestion(state).game);
-      assert.equal(state.questionNumber + 1, getNextQuestion(state).questionNumber);
+      const verifiedState = getNextQuestion(state);
+      assert.notEqual(-1, verifiedState.game);
+      assert.equal(state.questionNumber + 1, verifiedState.questionNumber);
     });
 
     it(`should check is last question`, () => {
       const state = {'questionNumber': 10, 'game': 0, 'isLastQuestion': false};
-      assert.equal(true, getNextQuestion(state).isLastQuestion);
-      assert.equal(10, getNextQuestion(state).questionNumber);
+      const verifiedState = getNextQuestion(state);
+      assert.equal(true, verifiedState.isLastQuestion);
+      assert.equal(10, verifiedState.questionNumber);
     });
   });
 });
